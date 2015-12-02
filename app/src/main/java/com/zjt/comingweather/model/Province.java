7892724d28724c01c2b762cfa0240d8f4de5121a
DecviL <2534450611@qq.com>
2015-12-02 23:41:45 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/zjt/comingweather/model/Province.java
@@ -0,0 +1,43 @@
+package com.zjt.comingweather.model;
+
+/**
+ * Created by Decvil on 2015/12/2.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Province{" +
+                "id=" + id +
+                ", provinceName='" + provinceName + '\'' +
+                ", provinceCode='" + provinceCode + '\'' +
+                '}';
+    }
+}
